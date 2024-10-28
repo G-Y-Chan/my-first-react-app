b55c6b5f320aca938f6892ec18bf895d336fef09
G-Y-Chan <changuanyu2904@gmail.com>
2024-10-29 01:30:03 +0800

    Create Avatar componenet

---
+++ src/Avatar.jsx
@@ -0,0 +1,11 @@
+function Avatar() {
+    return (
+        <img 
+            className="avatar"
+            src="https://i.imgur.com/NAGTvvz.png"
+            alt="Example user"
+        />
+    );
+}
+
+export default Avatar;
