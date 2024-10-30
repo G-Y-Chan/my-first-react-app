acf2810944298b1ee10d3ea72c11dd8bc9bdde2a
G-Y-Chan <changuanyu2904@gmail.com>
2024-10-30 19:12:03 +0800

    Create variable that maps array of pets to "li" items

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -2,6 +2,7 @@ function Avatar() {
     const avatar = 'https://i.imgur.com/NAGTvvz.png';
     const description = 'Example user';
     const pets = ["Lion", "Cow", "Snake", "Lizard"];
+    const petsList = pets.map((pet) => <li key={pet}>{pet}</li>);
     return (
         <div>
             <h1>User Information</h1>
