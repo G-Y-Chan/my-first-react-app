5c94f55e0f47ed117db186532d50cb5046165e96
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-01 13:03:35 +0800

    Create default display for sculptures

--- src/SculptureDisplay.jsx
+++ src/SculptureDisplay.jsx
@@ -2,5 +2,23 @@ import { useState } from "react";
 import { sculptureList } from "./data/SculptureList";
 
 function SculptureDisplay() {
-    
+    const [sculptureIndex, setSculptureIndex] = useState(0);
+
+    return (
+        <div>
+            <h1>{sculptureList[sculptureIndex].name}</h1>
+            <h2>
+                By: {sculptureList[sculptureIndex].artist}
+            </h2>
+            <img
+                src={sculptureList[sculptureIndex].url}
+                alt={sculptureList[sculptureIndex].alt}
+            ></img>
+            <p>
+                {sculptureList[sculptureIndex].description}
+            </p>
+        </div>
+    )
 }
+
+export default SculptureDisplay;
