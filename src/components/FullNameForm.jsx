5ab325d26d995935ee753831dd068aae19e3dc87
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-02 01:45:29 +0800

    Create empty div to store components of form

--- src/components/FullNameForm.jsx
+++ src/components/FullNameForm.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 function FullNameForm() {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    return (
+        <div>
+
+        </div>
+    );
 }
