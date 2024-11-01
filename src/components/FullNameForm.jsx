d07eb61354a91c66cc6d22d95ac0f32615512631
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-02 01:49:44 +0800

    Create functions to set first/last name states

--- src/components/FullNameForm.jsx
+++ src/components/FullNameForm.jsx
@@ -4,6 +4,14 @@ function FullNameForm() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
 
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value);
+    }
+
+    function handleLastNameChange(e) {
+        setLastName(e.target.value);
+    }
+
     return (
         <div>
 
