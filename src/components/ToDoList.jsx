b1d8e9969f6373cfe16aab736d45927dc82bc7d4
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-03 12:39:39 +0800

    Add input and buttons elements to add new tasks

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,6 +3,9 @@ import ToDoItem from "./ToDoItem";
 function ToDoList(props) {
     return (
         <div>
+            <h2>Add new task:</h2>
+            <input></input>
+            <button>Submit</button>
             {props.toDoList.map((item) => {
                 return <ToDoItem toDo={item} key={item.id}></ToDoItem>;
             })}
