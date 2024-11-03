f307e05660afef8057da249cfc7443355c586669
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-03 13:13:28 +0800

    Add function to add new task to the list

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,12 +1,33 @@
 import ToDoItem from "./ToDoItem";
+import { useState } from "react";
+
+const newTask = {}
+
+function handleSubmitEvent(taskList, setTaskList, newTask) {
+    if (newTask.hasOwnProperty("title")) {
+        newTask.id = taskList.length;
+        setTaskList([...taskList, newTask]);
+    }
+}
 
 function ToDoList(props) {
+    const [taskList, setTaskList] = useState(props.toDoList);
+    console.log(taskList);
+
     return (
         <div>
             <h2>Add new task:</h2>
-            <input></input>
-            <button>Submit</button>
-            {props.toDoList.map((item) => {
+            <input
+                onChange={(e) => {
+                    newTask.title = e.target.value;
+                }}
+            ></input>
+            <button
+                onClick={() => handleSubmitEvent(taskList, setTaskList, newTask)}
+            >
+                Submit
+            </button>
+            {taskList.map((item) => {
                 return <ToDoItem toDo={item} key={item.id}></ToDoItem>;
             })}
         </div>
